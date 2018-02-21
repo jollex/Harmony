b1ed38595eadb09e195aea20f9ab8a6d2112d6a4
root <root@jollex6.localdomain>
2018-02-20 19:06:02 -0500

    Fix BiblioPixel imports in main

--- test.py
+++ test.py
@@ -4,9 +4,6 @@ from bibliopixel.drivers.channel_order import ChannelOrder
 
 driver = SPI(ledtype='LPD8806', num=24, c_order=ChannelOrder.GRB)
 led = Strip(driver)
-
 print('Loaded')
-
-while True:
-    led.fill((209, 36, 44))
-    print('Filled')
+led.fill((209, 36, 44))
+print('Filled')
