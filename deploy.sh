1747bf1210725e60af290dc0d8777b007e4827cf
Alex Jolly <alex@alexjolly.me>
2018-08-19 01:54:56 -0400

    Remove deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-USER_HOST=pi@192.168.0.12
-
-ssh pi@192.168.0.12 "sudo systemctl stop discord-light.service"
-rsync --exclude=.idea --exclude=.git -av . ${USER_HOST}:~/DiscordLight/
-ssh pi@192.168.0.12 "sudo systemctl start discord-light.service"
